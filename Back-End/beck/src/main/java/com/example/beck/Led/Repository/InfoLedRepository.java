f00bab87fa824c902df6b7cb5f7bcb7c9dad03e2
Sant-Thiago <thiagovieirab2b@gmail.com>
2024-08-01 19:38:01 -0300

    removendo arquivos

--- Back-End/beck/src/main/java/com/example/beck/Led/Repository/InfoLedRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.beck.Led.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.example.beck.Led.Model.InfoLed;
-
-@Repository
-public interface InfoLedRepository extends JpaRepository<InfoLed, Long>{}
--- Back-End/beck/src/main/java/com/example/beck/Led/Repository/LedRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.beck.Led.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.example.beck.Led.Model.Led;
-
-@Repository
-public interface LedRepository extends JpaRepository<Led, Long> {}
